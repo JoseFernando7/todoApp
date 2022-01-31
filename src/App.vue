310951102520429c82043bcccf506bed50946ac4
Jose Fernando Restrepo Sanchez <jose.f72000@gmail.com>
2022-01-31 17:55:34 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default 
+{
+  name: 'App',
+  components:
+  {
+    Todo
+  }
+}
+</script>
+
+<style>
+
+</style>
