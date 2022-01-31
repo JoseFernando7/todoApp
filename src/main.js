0a46f13f139189b0f18fcc284531228d8f7afd47
Jose Fernando Restrepo Sanchez <jose.f72000@gmail.com>
2022-01-31 17:59:30 -0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,8 @@
+//import Vue from 'vue';
+import { createApp } from 'vue';
+import App from './App.vue';
+//import Todo from './components/Todo.vue';
+
+//Vue.component('todo', Todo);
+
+createApp(App).mount('#app')
